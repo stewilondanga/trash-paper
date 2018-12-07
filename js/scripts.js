bf09c8008d7f923e585e47b1d856a1385c28b1a3
Stewart ilondanga <stilondanga@gmail.com>
2018-12-07 09:28:44 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,21 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+const passwordStrength = function() {
+  let userInput = document.getElementById("password");
+  let messageField = document.getElementById("cross");
+  messageField.style.visibility = "visible";
+  messageField.innerText = "X";
+  messageField.style.color = " #ff2323";
+  const passwordCheck = /^([a-zA-Z0-9]{8,12})$/;
+  if (userInput.value.match(passwordCheck)) {
+    messageField.innerText = ":)";
+    messageField.style.color = "#297730";
+  }
+};
+
+$("button").click(function() {
+  $(".wrapper").fadeOut("slow");
+  $(".button").fadeOut("fast");
+});
